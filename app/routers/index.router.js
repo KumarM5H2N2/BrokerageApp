f3dae678383d09a36ca51c8911fd8aaf1a47997d
KumarNatesan <kumarnatesan75@gmail.com>
2024-06-09 23:23:45 +0200

    writing a not found middleware 404

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -1,7 +1,10 @@
 import {Router} from 'express';
 const router = Router();
 
-
+// Not Found Middleware (404)
+router.use((req, res) => {
+    res.status(404).json({ error: "Ressource not found" });
+  });
 
 
 export default router;
