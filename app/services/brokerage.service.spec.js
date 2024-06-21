6ff568d8856d92742a92550fce83c97b0403182c
KumarM5H2N2 <kumarnatesan75@gmail.com>
2024-06-21 12:04:25 +0200

    an example of error with JEST with spec file

--- app/services/brokerage.service.spec.js
+++ app/services/brokerage.service.spec.js
@@ -5,3 +5,14 @@ test('brokerage is an object', ()=>{
  expect(typeof brokerageService).toBe('object');
 
 });
+
+test('brokerage function exists', ()=>{
+ expect(typeof brokerageService.generate).toBe('function');
+
+});
+
+// An example generate an error 
+test('generate return an object', ()=>{
+    expect(typeof brokerageService.generate).toBe('object');
+   
+   });
