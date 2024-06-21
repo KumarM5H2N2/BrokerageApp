2a9fd89820da64dacfb9a1ea4a3fb0795c2b6b69
KumarM5H2N2 <kumarnatesan75@gmail.com>
2024-06-21 08:37:01 +0200

    implementing first route and test.http file on use

---
+++ app/controllers/index.controller.js
@@ -0,0 +1,8 @@
+export default {
+
+getBrokerage(req, res) {
+    res.json({});
+},
+
+
+};
--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -1,10 +1,18 @@
 import {Router} from 'express';
+import controller from '../controllers/index.controller.js';
 const router = Router();
 
+
+
+router.get('/brokerage', controller.getBrokerage);
+
+
+
+/*
 // Not Found Middleware (404)
 router.use((req, res) => {
     res.status(404).json({ error: "Ressource not found" });
   });
-
+*/
 
 export default router;
